49ea5fc6966e284fa93c2a4fd36421d21d49bae5
justintoblack <870582311@qq.com>
2022-04-20 19:25:39 +0800

    test

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,28 @@
+/**
+ * @create 2022/4/20-16:25
+ */
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.Date;
+
+@WebServlet(name = "HelloServlet", value = "/HelloServlet")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            response.getWriter().println("<h1>Hello Servlet!</h1>");
+            response.getWriter().println(new Date());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
